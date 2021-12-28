e219cf4faec28842e1b78a293b251fd93ea15af3
staykimin <67236270+staykimin@users.noreply.github.com>
2021-12-28 19:08:32 +0700

    Add files via upload

--- Machine Learning/allin/index.php
+++ Machine Learning/allin/index.php
@@ -1,40 +1,132 @@
-
-<?php
-require("bin/core.php");
-
-$x = array(24,22,21,20,22,19,20,23,24,25,21,20,20,19,25,27,28,25,26,24,27,23,24,23,22,21,26,25,26,27);
-$y = array(10,5,6,3,6,4,5,9,11,13,7,4,6,3,12,13,16,12,14,12,16,9,13,11,7,5,12,11,13,14);
-$n = sizeof($y);
-
-$sin = new Core($x, $y, 50);
-$cos = new Phase1($x, $y);
-
-echo "<br>Rata - Rata X		 : ".$cos->mean($x);
-echo "<br>Rata - Rata Y		 : ".$cos->mean($y);
-
-echo "<br>Regresi Linear (C) : ".$sin->Regresi_Linear_C();
-echo "<br>Regresi Linear (R) : ".$sin->Regresi_Linear_R();
-echo "<br>Hasil Predisksi 	 : ".$sin->Prediksi();
-
-?>
-
+<!DOCTYPE html>
 <html>
-	<body>
-
-		<table>
-			<tr>
-				<td>NO</td>
-				<td>X</td>
-				<td>Y</td>>
-			</tr>
-		<?php for($i=0; $i<$n; $i++):?>
-		<tr>
-			<td><?php echo $i+1;?></td>
-			<td><?php echo $x[$i];?></td>
-			<td><?php echo $y[$i];?></td>
-			
-		</tr>
-		<?php endfor; ?>
-		</table>
-	</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {
+              clifford: "#da373d",
+            },
+          },
+        },
+      };
+    </script>
+    <style type="text/tailwindcss">
+      @layer utilities {
+        .content-auto {
+          content-visibility: auto;
+        }
+      }
+    </style>
+  </head>
+  <body class="antialiased font-roboto bg-gray-200">
+    <div class="flex h-screen justify-center items-center">
+      <div class="w-1/3 bg-gray-100 border shadow-lg rounded-lg p-6">
+        <!-- form -->
+        <form action="bin/main.php" method="post" enctype="multipart/form-data">
+          <div class="mb-4">
+            <label for="input1" class="block text-base font-medium text-gray-500 mb-2">
+              Prediksi data ke-
+            </label>
+            <input
+              type="number"
+			  name ="menuju"
+              class="
+                transation-colors
+                duration-300
+                border border-blue-400
+                rounded-lg
+                focus:border-blue-600 focus:outline-none
+                w-full
+                px-2
+                py-2
+              "
+              placeholder="input your number"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="input1" class="block text-base font-medium text-gray-500 mb-2">
+              Nilai ke X :
+            </label>
+            <input
+              type="text"
+			  name="nilaix"
+              class="
+                transation-colors
+                duration-300
+                border border-blue-400
+                rounded-lg
+                focus:border-blue-600 focus:outline-none
+                w-full
+                px-2
+                py-2
+              "
+              placeholder="input your number"
+            />
+          </div>
+          <div class="mb-4">
+            <label for="input1" class="block text-base font-medium text-gray-500 mb-2">
+              Nilai ke Y :
+            </label>
+            <input
+              type="text"
+			  name="nilaiy"
+              class="
+                transation-colors
+                duration-300
+                border border-blue-400
+                rounded-lg
+                focus:border-blue-600 focus:outline-none
+                w-full
+                px-2
+                py-2
+              "
+              placeholder="input your number"
+            />
+          </div>
+          <div>
+            <button
+              class="
+                flex
+                items-center
+                inline-flex
+                btn
+                bg-blue-500
+                hover:bg-blue-700
+                py-2
+                px-3
+                rounded
+                shadow-lg
+                text-white text-base
+                font-semibold
+              "
+              type="submit"
+			  name="excute"
+            >
+              <svg
+                class="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                />
+              </svg>
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
 </html>
